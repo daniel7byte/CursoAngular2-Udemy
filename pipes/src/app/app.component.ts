83d8f3c2a63a5ee49572dd7b05096d0ba7878065
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-06-08 23:34:16 -0500

    Pipes 1

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = "Jose Daniel";
+
+  arreglo = [1, 2 ,3 ,4 , 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI ;
+
+  a = Math.random();
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre:"Logan",
+    clave:"Wolverin",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "14"
+    }
+  };
+
+  valorPromesa = new Promise( (resolve, reject) => {
+    setTimeout(() => {resolve('Llegó la data!'),3500});
+  });
+
+  fecha = new Date;
+
+}
