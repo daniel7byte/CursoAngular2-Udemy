29cd3bcfd061845ea3e2aba245dde11a3484672b
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-06-09 14:51:42 -0500

    SpotifyApp 3

--- SpotifyApp/src/app/app.module.ts
+++ SpotifyApp/src/app/app.module.ts
@@ -11,13 +11,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
+    SinfotoPipe,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
--- SpotifyApp/src/app/services/spotify.service.ts
+++ SpotifyApp/src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
 
     // Be careful!
     let headers = new Headers();
-    headers.append( 'authorization' , 'Bearer BQDNwtlxv5160GxTxPWUI1qQakP9zyKY-fbw1nnMbXWo8h8TAFBRvo_T908GB6-X8Y6Aole-qi1DNyhqDLPjBA');
+    headers.append( 'authorization' , 'Bearer BQBblUcW_pqr6bwVAx5Eyb3SquuhoPkYBNEkzzswT9bu2dbxgKwgn7CLyVSqBbhYUpGeea-C_FyK924oPjBqUQ');
 
     let query = `?q=${termino}&type=artist`;
     let url = this.urlBusqueda + query;
@@ -25,6 +25,24 @@ export class SpotifyService {
               // console.log(res.json());
               this.artistas = res.json().artists.items;
               console.log(this.artistas);
+              // return this.artistas;
+            });
+  }
+
+  getArtista( id:string ) {
+
+    // Be careful!
+    let headers = new Headers();
+    headers.append( 'authorization' , 'Bearer BQBblUcW_pqr6bwVAx5Eyb3SquuhoPkYBNEkzzswT9bu2dbxgKwgn7CLyVSqBbhYUpGeea-C_FyK924oPjBqUQ');
+
+    let query = `/${id}`;
+    let url = this.urlArtista + query;
+
+    return this._http.get( url, {headers} ).map( res => {
+              // console.log(res.json());
+              return res.json();
+              // this.artistas = res.json().artists.items;
+              // console.log(this.artistas);
               // return this.artistas;
             });
   }
